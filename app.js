99ece6bab5d1ce9493545440029f02e587a36b71
hitartha-gogoi <arnabgogoi83@gmail.com>
2024-12-28 10:12:23 +0530

    google-login

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(session({
   passport.use(new GoogleStrategy({
    clientID: process.env.GOOGLE_CLIENT_ID,
    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL: `http://localhost:8080/auth/google/callback`, // Redirect URI
+   callbackURL: `/auth/google/callback`, // Redirect URI
  },
  (accessToken, refreshToken, profile, done) => {
    // Here you can save user details to a database
